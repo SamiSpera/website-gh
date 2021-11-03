a3ae076d3ed86e26027795ec2ba0eefade4b0644
SamiSpera <samantha@docspera.com>
2021-11-03 14:29:47 -0400

    try loading from google fonts

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,9 @@ export default function Home() {
         <title>DocSpera | HIPAA Compliant, Integrated Surgical Coordination Platform</title>
         <link rel='icon' href='/website-gh/favicon.ico' />
         <link rel='preload' href='/website-gh/fonts/VarelaRound-Regular.ttf' as='font' crossOrigin='' />
-        <link href="/website-gh/fonts/style.css" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet"></link>
       </Head>
       <HeaderNav />
 
