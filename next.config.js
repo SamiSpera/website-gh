2b69ee4b435547bf5a310b3211985151c40cf815
Luca Santarella <luca.santarella@gmail.com>
2021-11-09 16:12:51 -0500

    Updated for docker deployment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-const debug = process.env.NODE_ENV !== 'production'
-
-console.log('DEBUG', debug)
-
 module.exports = {
   // basePath: '/website-gh',
   exportPathMap: function () {
@@ -13,7 +9,7 @@ module.exports = {
       '/': { page: '/' }
     }
   },
-  assetPrefix: !debug ? '/website-gh' : '',
+  assetPrefix: '',
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
