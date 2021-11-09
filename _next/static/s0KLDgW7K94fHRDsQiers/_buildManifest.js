f9e0ccf8ac998a09eca9dc0449d5f4dd9a725a61
SamiSpera <samantha@docspera.com>
2021-11-09 11:45:32 -0500

    Updates

---
+++ _next/static/s0KLDgW7K94fHRDsQiers/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/919-cd988f0a280e4b9952de.js","static/chunks/pages/index-915d57e25e336c360d0e.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
