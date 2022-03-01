3ad1efc08e3ef5f2b80bcbff0e44fa80b05a0a09
Ogheneovo Eko <ovoe@decagonhq.com>
2022-03-01 19:20:34 +0100

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useRouter } from 'next/router'
 import { ContextProvider } from '../context/context'
 import '../styles/global.css'
 
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
     <ContextProvider>
       <Component {...pageProps} />
